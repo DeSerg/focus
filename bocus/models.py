f41e47b72e2f0008cca470c226ad1c60ff1ed643
sergei.popov <sergei.popov@corp.mail.ru>
2020-06-11 12:59:02 +0300

    WIP

--- bocus/models.py
+++ bocus/models.py
@@ -1,9 +1,25 @@
 from django.db import models
+import string
+import random
+import os
+
+def random_choice():
+    alphabet = string.ascii_lowercase + string.digits
+    return ''.join(random.choices(alphabet, k=8))
 
 def images_path():
         return os.path.join(settings.LOCAL_FILE_DIR, 'images')
 
 # Create your models here.
 class Album(models.Model):
-    path = models.FilePathField(path=images_path, allow_folders=True)
+    name = models.CharField(max_length=20, default=random_choice())
+    path = models.CharField(max_length=100)
+
+    def photos_num(self):
+        count = 0
+        for name in os.listdir(self.path):
+            if os.path.isfile(os.path.join(self.path, name)):
+                count += 1
+        return count
+
 
--- bocus/views.py
+++ bocus/views.py
@@ -1,6 +1,89 @@
+import os
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import FormView
+from django.core.files.storage import default_storage
+
+from .forms import UploadPhotosForm, RecognizeForm
+from .models import Album
+
+import bocus.extern as ext
+import tempfile
+from zipfile import ZipFile
+from .recognize import RecognizeFace
+
 
 def index(request):
-        return HttpResponse("Hello, world. You're at the polls index.")
+    album_list = Album.objects.all()
+    context = {'album_list': album_list}
+    return render(request, 'bocus/index.html', context)
+
+def create_album(request):
+    album = Album()
+    album.path = os.path.join(ext.IMAGES_DIR, album.name)
+    os.mkdir(album.path)
+    album.save()
+    return HttpResponseRedirect(reverse('bocus:detail', args=(album.id,)))
+
+def detail(request, album_id):
+    album = Album.objects.get(pk=album_id)
+    upload_photos_form = UploadPhotosForm()
+    recognize_form = RecognizeForm()
+
+    context = {'album': album,
+            'photos_num': album.photos_num(),
+            'upload_photos_form': upload_photos_form,
+            'recognize_form': recognize_form}
+
+    return render(request, 'bocus/album.html', context)
+
+def upload_photos(request, album_id):
+    album = Album.objects.get(pk=int(album_id))
+    if request.method == 'POST':
+        print('Files: {}'.format(request.FILES))
+        form = UploadPhotosForm(request.POST, request.FILES)
+        if form.is_valid():
+            files = request.FILES.getlist('file_field')
+            for f in files:
+                image_path = os.path.join(album.path, f.name)
+                print('Saving file {}'.format(image_path))
+                with open(image_path, 'wb+') as destination:
+                    for chunk in f.chunks():
+                        destination.write(chunk)
+
+        else:
+            print('Invalid form! {}'.format(form.errors))
+
+    return HttpResponseRedirect(reverse('bocus:detail', args=(album.id,)))
+
+def recognize(request, album_id):
+    album = Album.objects.get(pk=int(album_id))
+
+    if request.method != 'POST':
+        return HttpResponseRedirect(reverse('bocus:detail', args=(album.id,)))
+
+    face_img_file = tempfile.NamedTemporaryFile()
+    face_img_path = face_img_file.name
+
+    upl_f = request.FILES['file']
+    for chunk in upl_f.chunks():
+        face_img_file.write(chunk)
+
+    recognizer = RecognizeFace(face_img_path, album.path)
+    filepaths = recognizer.perform_recognition()
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        archive_path = os.path.join(tmpdirname, 'recognized.zip')
+        zipObj = ZipFile(archive_path, 'w')
+
+        for filepath in filepaths:
+            zioObj.write(filepath)
+
+        zipObj.close()
+
+    response = FileResponse(open(archive_path, 'rb'))
+    return response
 
